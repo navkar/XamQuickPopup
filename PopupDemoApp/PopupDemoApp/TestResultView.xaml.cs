84cc6f4cd83275dfae6ae8e510a2f0069c8709f9
Naveen Kar <nkaramchetti@ups.com>
2018-10-15 14:45:43 -0400

    Initial version

---
+++ PopupDemoApp/PopupDemoApp/TestResultView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PopupDemoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestResultView
+    {
+        public TestResultView()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
